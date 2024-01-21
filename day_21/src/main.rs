0f40ce97a0f182c6ee88e1552196efc9647e5b59
Aniket <aniketbotre007@gmail.com>
2024-01-22 00:01:41 +0530

    Add day_21 module with Liftimes concept

---
+++ day_21/src/main.rs
@@ -0,0 +1,10 @@
+#[derive(Debug)]
+struct Highlight<'document>(&'document str);
+
+fn main() {
+    let text = String::from("The quick brown fox jumps over the lazy dog.");
+    let fox = Highlight(&text[4..19]);
+    let dog = Highlight(&text[35..43]);
+    println!("{:?}", fox);
+    println!("{:?}", dog);
+}
