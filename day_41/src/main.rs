66bddd792d14887720426d6613803ea0dbe67843
Aniket <aniketbotre007@gmail.com>
2024-02-11 23:32:47 +0530

    Add day_41 with Actix-Web implementation middleware

---
+++ day_41/src/main.rs
@@ -0,0 +1,25 @@
+use actix_web::{
+    get, http::header::ContentEncoding, middleware, App, HttpResponse, HttpServer,
+};
+
+#[get("/")]
+async fn index() -> HttpResponse {
+    HttpResponse::Ok()
+        // v- disable compression
+        .insert_header(ContentEncoding::Identity)
+        .body("data")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server running at http://localhost:8080/");
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Compress::default())
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
