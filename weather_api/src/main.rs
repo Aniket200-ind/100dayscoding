f052218a9d0d05ffcd31ee94705066665d754c6c
Aniket <aniketbotre007@gmail.com>
2024-02-15 22:10:48 +0530

    Implement Weather API and prepare for Air Quality Index API

--- weather_api/src/main.rs
+++ weather_api/src/main.rs
@@ -1,23 +1,37 @@
-use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use reqwest::Client;
 use std::env;
-
 mod models;
-use crate::models::Weather;
+use models::Weather;
+use models::AirQualityIndex;
+use models::aqi::AirQuality;
+
 
 #[get("/")]
 async fn index() -> impl Responder {
     let api_key = env::var("OPENWEATHER_API_KEY").expect("OPENWEATHER_API_KEY must be set");
     let city = "Mumbai";
 
-    todo!("Implement a another api for AQI => https://openweathermap.org/api/air-pollution");
+    // Weather API
+    let weather_url = format!(
+        "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric",
+        city, api_key
+    );
+    let weather_response = Client::new().get(&weather_url).send().await.unwrap();
+todo!("Parse the JSON response into a Weather struct and return it as json response. Also make sure to use lat and lon from the Weather struct to call the Air Quality Index API.");
+    let body = weather_response.text().await.unwrap();
 
-    let url = format!("https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric", city, api_key);
-    let response = Client::new().get(&url).send().await.unwrap();
-    let body = response.text().await.unwrap();
+    // Air Quality Index API
+    // let aqi_url = format!(
+    //     "https://api.openweathermap.org/data/2.5/air_pollution?lat={}&lon={}&appid={}",
+    //     lat, lon, api_key
+    // );
+    // let aqi_response = Client::new().get(&url).send().await.unwrap();
+    // let body = response.text().await.unwrap();
 
     HttpResponse::Ok().body(body)
 }
+
 
 
 #[actix_web::main]
@@ -30,7 +44,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(index)
     })
-    .bind("localhost:8080")?
-    .run()
-    .await
+        .bind("localhost:8080")?
+        .run()
+        .await
 }
