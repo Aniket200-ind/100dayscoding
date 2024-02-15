f7862b6eb66c9e1fa2d7b1078297d7fa8a9a571b
Aniket <aniketbotre007@gmail.com>
2024-02-15 22:09:31 +0530

    Refactor Weather struct and its related models

--- weather_api/src/models/weather.rs
+++ weather_api/src/models/weather.rs
@@ -1,36 +1,36 @@
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
-pub struct Weather{
-    coord: Coord,
-    weather: Vec<WeatherData>,
-    main: Main,
-    wind: Wind,
-    name: String
+pub struct Weather {
+    pub coord: Coord,
+    pub weather: Vec<WeatherData>,
+    pub main: Main,
+    pub wind: Wind,
+    pub name: String,
 }
 
-#[derive(Deserialize)]
-pub struct Coord{
-    lon: f32,
-    lat: f32
+#[derive(Deserialize, Debug)]
+pub struct Coord {
+    pub lon: f32,
+    pub lat: f32,
 }
 
-#[derive(Deserialize)]
-pub struct WeatherData{
-    main: String,
-    description: String
+#[derive(Deserialize, Debug)]
+pub struct WeatherData {
+    pub main: String,
+    pub description: String,
 }
 
-#[derive(Deserialize)]
-pub struct Main{
-    temp: f32,
-    feels_like: f32,
-    temp_min: f32,
-    temp_max: f32,
-    humidity: i32
+#[derive(Deserialize, Debug)]
+pub struct Main {
+    pub temp: f32,
+    pub feels_like: f32,
+    pub temp_min: f32,
+    pub temp_max: f32,
+    pub humidity: i32,
 }
 
-#[derive(Deserialize)]
-pub struct Wind{
-    speed: f32,
+#[derive(Deserialize, Debug)]
+pub struct Wind {
+    pub speed: f32,
 }
