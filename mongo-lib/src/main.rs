68ea00a7ac3359d8597b29e06e17c70ac0e1d32f
Aniket <aniketbotre007@gmail.com>
2024-02-29 18:55:06 +0530

    Add CRUD routes for books

--- mongo-lib/src/main.rs
+++ mongo-lib/src/main.rs
@@ -16,5 +16,38 @@ type WebResult<T> = std::result::Result<T, Rejection>;
 async fn main() -> Result<()> {
     let db = DB::init().await?;
     let books = warp::path("books");
+
+    let book_routes = books
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(with_db(db.clone()))
+        .and_then(handlers::create_book_handler)
+        .or(books
+            .and(warp::put())
+            .and(warp::path::param())
+            .and(warp::body::json())
+            .and(with_db(db.clone()))
+            .and_then(handlers::update_book_handler)
+        )
+        .or(books
+            .and(warp::delete())
+            .and(warp::path::param())
+            .and(with_db(db.clone()))
+            .and_then(handlers::delete_book_handler)
+        )
+        .or(books
+            .and(warp::get())
+            .and(with_db(db.clone()))
+            .and_then(handlers::books_list_handler)
+        );
+
+    let routes = book_routes.recover(error::handle_rejection);
+    println!("Starting server at http://localhost:8080");
+    warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
+    Ok(())
+}
+
+fn with_db(db: DB) -> impl Filter<Extract = (DB,), Error = Infallible> + Clone {
+    warp::any().map(move || db.clone())
 }
 
