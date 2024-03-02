25a342cb5df2949cc56a23c3b86b300aa0eedf3c
Aniket <aniketbotre007@gmail.com>
2024-03-02 18:43:35 +0530

    Refactor book handlers

--- mongo-lib/src/handlers.rs
+++ mongo-lib/src/handlers.rs
@@ -12,22 +12,22 @@ pub struct BookRequest {
     pub tags: Vec<String>,
 }
 
-pub async fn books_list_handler(db: DB) -> WebResult<impl Reply>{
+pub async fn books_list_handler(db: DB) -> WebResult<impl Reply> {
     let books = db.fetch_books().await.map_err(|e| reject::custom(e))?;
     Ok(json(&books))
 }
 
-pub async fn create_book_handler(book: BookRequest, db: DB) -> WebResult<impl Reply>{
-    db.create_book(book).await.map_err(|e| reject::custom(e))?;
+pub async fn create_book_handler(book: BookRequest, db: DB) -> WebResult<impl Reply> {
+    db.create_book(&book).await.map_err(|e| reject::custom(e))?;
     Ok(StatusCode::CREATED)
 }
 
-pub async fn update_book_handler(id: String, book: BookRequest, db: DB) -> WebResult<impl Reply>{
-    db.update_book(id, book).await.map_err(|e| reject::custom(e))?;
+pub async fn update_book_handler(id: String, book: BookRequest, db: DB) -> WebResult<impl Reply> {
+    db.update_book(id, &book).await.map_err(|e| reject::custom(e))?;
     Ok(StatusCode::NO_CONTENT)
 }
 
-pub async fn delete_book_handler(id: String, db: DB) -> WebResult<impl Reply>{
+pub async fn delete_book_handler(id: String, db: DB) -> WebResult<impl Reply> {
     db.delete_book(id).await.map_err(|e| reject::custom(e))?;
     Ok(StatusCode::NO_CONTENT)
 }
