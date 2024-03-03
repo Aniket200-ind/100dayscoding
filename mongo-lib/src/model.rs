51dec3998083c211e1b00658a2ae0ada0d600622
Aniket <aniketbotre007@gmail.com>
2024-03-03 11:14:51 +0530

    Remove unused fields in Book struct

--- mongo-lib/src/model.rs
+++ mongo-lib/src/model.rs
@@ -1,4 +1,3 @@
-use chrono::{DateTime, Utc};
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -7,8 +6,7 @@ pub struct Book {
     pub title: String,
     pub author: String,
     pub num_pages: usize,
-    pub price: f64,
-    pub in_stock: bool,
+    // pub price: f64,
+    // pub in_stock: bool,
     pub tags: Vec<String>,
-    pub added_at: DateTime<Utc>,
 }
