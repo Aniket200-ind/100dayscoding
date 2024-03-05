f791ce3ed7482a584209f90dece96d0ab32c7e22
Aniket <aniketbotre007@gmail.com>
2024-03-05 22:35:04 +0530

    Add models for user authentication

---
+++ jwt-auth/src/models.rs
@@ -0,0 +1,25 @@
+use serde::{Deserialize, Serialize};
+
+pub enum Role {
+    Admin,
+    User,
+}
+
+#[derive(Clone)]
+pub struct User {
+    pub uid: String,
+    pub email: String,
+    pub password: String,
+    pub role: Role,
+}
+
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Serialize)]
+pub struct LoginResponse {
+    pub token: String,
+}
