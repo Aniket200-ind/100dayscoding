bf6545a2c5f05fe2811adb35a89c2489c5fc54a4
Aniket <aniketbotre007@gmail.com>
2024-03-06 22:12:59 +0530

    Add user and admin handlers, and update login handler

--- jwt-auth/src/handlers.rs
+++ jwt-auth/src/handlers.rs
@@ -14,15 +14,24 @@ pub fn with_users(user: User) -> impl Filter<Extract = (User,), Error = Infallib
     warp::any().map(move || user.clone())
 }
 
+pub async fn user_handler(uid: String) -> WebResult<impl Reply> {
+    Ok(format!("Hello, user with id {}", uid))
+}
+pub async fn admin_handler(uid: String) -> WebResult<impl Reply> {
+    Ok(format!("Hello, admin with id {}", uid))
+}
+
 pub async fn login_handler(users: User, body: LoginRequest) -> WebResult<impl Reply> {
     match users
         .iter()
         .find(|(_uid, user)| user.email == body.email && user.password == body.password)
     {
         Some((uid, _user)) => {
-
+            let token = auth::create_jwt(&uid, &Role::from_str(&user.role))
+                .map_err(|e| reject::custom(e))?;
+            Ok(reply::json(&LoginResponse { token }))
         }
-        None => Err(reject::custom(error::Error::Unauthorized)),
+        None => Err(reject::custom(WrongCredentialsError)),
     }
 }
 
