24c449799f73ed11169a632f89c1467c0519f23d
Aniket <aniketbotre007@gmail.com>
2024-03-14 23:36:57 +0530

    Refactor main.rs to use a switch statement for routing

--- yew-router/src/main.rs
+++ yew-router/src/main.rs
@@ -1,11 +1,29 @@
-mod route;
 mod components;
+mod route;
 
+use components::navbar::Navbar;
+use route::Route;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html!(<components::home::Home />),
+        Route::About => html!(<components::about::About />),
+        Route::Contact => html!(<components::contact::Contact />),
+    }
+}
 
+#[function_component(Main)]
+pub fn app() -> Html {
+    html!(
+        <BrowserRouter>
+            <Navbar />
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    )
+}
 
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<Main>::new().render();
 }
