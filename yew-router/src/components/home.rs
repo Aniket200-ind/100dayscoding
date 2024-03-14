7f99d9c7b147609eb09c6537e1031068ce71f13e
Aniket <aniketbotre007@gmail.com>
2024-03-14 23:37:29 +0530

    Add Home component to display the home page

--- yew-router/src/components/home.rs
+++ yew-router/src/components/home.rs
@@ -1,2 +1,13 @@
 use yew::prelude::*;
 
+#[function_component(Home)]
+pub fn home() -> Html {
+    html! {
+        <div class="
+        flex justify-center items-center flex-col h-screen bg-delft-blue text-white p-4
+        ">
+            <h1 class="text-5xl font-bold"> { "Home" } </h1>
+            <p class="mt-4 text-2xl"> { "Welcome to the home page." } </p>
+        </div>
+    }
+}
