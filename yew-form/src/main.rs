1e66d054399ed5b5980239ec4d8847feb27cad82
Aniket <aniketbotre007@gmail.com>
2024-03-16 23:46:12 +0530

    Add custom form component and update main function

--- yew-form/src/main.rs
+++ yew-form/src/main.rs
@@ -1,3 +1,19 @@
+mod components;
+
+use yew::prelude::*;
+use components::custom_form::CustomForm;
+
+#[function_component(App)]
+fn app() -> Html{
+    html!(
+        // use tailwind css for styling
+        <section class="flex flex-col space-y-6 bg-black justify-center items-center h-screen">
+            <h1 class="text-4xl font-bold text-platinum font-serif">{"User Form"}</h1>
+            <CustomForm />
+        </section>
+    )
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
