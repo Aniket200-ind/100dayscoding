dac6cf339f9bf88473b195074fb24bacb1c016a5
Aniket <aniketbotre007@gmail.com>
2024-03-19 22:27:57 +0530

    Add string reversal functionality to main.rs

--- reverse-string/src/main.rs
+++ reverse-string/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut input_string = String::new();
+    println!("Enter a string to reverse it: ");
+    std::io::stdin().read_line(&mut input_string).unwrap();
+    let reversed_string = reverse(&input_string);
+    println!("Reversed string: {}", reversed_string);
+}
+
+fn reverse(input: &str) -> String{
+    input.chars().rev().collect()
 }
