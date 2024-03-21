8fb3595f6a14546e566b8ee14784ff8cc53f5ed2
Aniket <aniketbotre007@gmail.com>
2024-03-21 23:09:08 +0530

    Add isogram checking functionality

--- isogram/src/main.rs
+++ isogram/src/main.rs
@@ -1,3 +1,25 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let mut word = String::new();
+    println!("Enter a word to check if it is an isogram:");
+    std::io::stdin().read_line(&mut word).expect("Failed to read line");
+    word = word.trim().to_string();
+    let is_isogram = check_isogram(&word);
+    match is_isogram {
+        Some(true) => println!("The word {} is an isogram", word),
+        Some(false) => println!("The word {} is not an isogram", word),
+        None => println!("The word {} is not an isogram", word),
+    }
+}
+
+fn check_isogram(word: &String) -> Option<bool>{
+    let mut seen_chars = HashSet::new();
+    for c in word.to_lowercase().chars(){
+        if seen_chars.contains(&c) {
+            return Some(false);
+        }
+        seen_chars.insert(c);
+    }
+    Some(true)
 }
