7b8a7bf364d4a3e26f9743d7199896667265c7e7
Aniket <aniketbotre007@gmail.com>
2024-03-25 10:15:45 +0530

    Add hashmap! macro to create HashMap

--- hashmap-macro/src/main.rs
+++ hashmap-macro/src/main.rs
@@ -1,3 +1,21 @@
-fn main(){
-    println!("Hello World!");
+#[macro_export]
+macro_rules! hashmap {
+    ($($key:expr => $value:expr),* $(,)?) => {
+        {
+            let mut map = std::collections::HashMap::new();
+            $(
+                map.insert($key, $value);
+            )*
+            map
+        }
+    };
+}
+
+fn main() {
+    let h_map = hashmap!{
+        "key1" => "value1",
+        "key2" => "value2",
+        "key3" => "value3",
+    };
+    println!("{:?}", h_map);
 }
